995387ff0b714fcfa44380f3d739f53733c3857f
Diego Saavedra <dsaavedra88@gmail.com>
2023-06-14 18:39:03 -0500

    Agregando Area de un rectangulo

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// Crear un script que calcule el area de un rectangulo
+
+double a = 0;
+double b = 0 ;
+double resultado = 0;
+
+Console.WriteLine("Por favor ingrese un valor");
+a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Por favor ingrese un valor");
+b = Convert.ToDouble(Console.ReadLine());
+
+resultado = a * b;
+
+Console.WriteLine("El resultado es: " + resultado);
